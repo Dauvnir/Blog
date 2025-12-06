e1736c168b959b2d84e59e7ddb7eb8b4875cfee0
Dauvnir <pdawidpolak@gmail.com>
2025-12-06 21:54:08 +0100

    Hero section desktop mock-up done

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,31 +2,31 @@ const API_URL = import.meta.env.PUBLIC_WP_URL;
 const WP_USER = import.meta.env.SECRET_WP_USER;
 const WP_PASSWORD = import.meta.env.SECRET_WP_PASSWORD;
 export async function APICall<T>(query: string): Promise<T> {
-	try {
-		if (!API_URL) throw new Error("Brak WP_URL w .env");
+  try {
+    if (!API_URL) throw new Error("Brak WP_URL w .env");
 
-		const headers: HeadersInit = { "Content-Type": "application/json" };
+    const headers: HeadersInit = { "Content-Type": "application/json" };
 
-		if (WP_PASSWORD && WP_USER) {
-			const token = btoa(`${WP_USER}:${WP_PASSWORD}`);
-			headers["Authorization"] = `Basic ${token}`;
-		}
-		const res = await fetch(API_URL, {
-			method: "POST",
-			headers: headers,
-			body: JSON.stringify({ query }),
-		});
+    if (WP_PASSWORD && WP_USER) {
+      const token = btoa(`${WP_USER}:${WP_PASSWORD}`);
+      headers["Authorization"] = `Basic ${token}`;
+    }
+    const res = await fetch(API_URL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({ query }),
+    });
 
-		const json = await res.json();
+    const json = await res.json();
 
-		if (json.errors) {
-			console.error("GraphQL error API", json.errors);
-			throw new Error("GraphQL error API", json.errors);
-		}
+    if (json.errors) {
+      console.error("GraphQL error API", json.errors);
+      throw new Error("GraphQL error API", json.errors);
+    }
 
-		return json.data;
-	} catch (error) {
-		console.error("Call failed");
-		throw error;
-	}
+    return json.data;
+  } catch (error) {
+    console.error("Call failed");
+    throw error;
+  }
 }
